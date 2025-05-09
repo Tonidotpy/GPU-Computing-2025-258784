4ffdef59c9c8530f0c6d660c816babf761c7c1f0
Antonio Gelain <antonio.gelain2@gmail.com>
2025-05-10 00:10:33 +0200

    feat: add csr gpu warp implmentation template

---
+++ deliverable-1/project/csr-gpu-warp/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=edu-short
+#SBATCH --nodes=1
+#SBATCH --tasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:05:00
+#SBATCH --job-name=csr-gpu-spmv
+#SBATCH --output=test-%j.out
+#SBATCH --error=test-%j.err
+
+module load CUDA/12.1
+srun build/SpMV ../data/1138_bus.mtx
